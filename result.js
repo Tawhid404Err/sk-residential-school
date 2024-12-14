41041dd9ed10913508895da0d26aadf9c047da30
Tawhid Islam <xcode.dev@yahoo.com>
2024-12-15 00:45:42 +0600

    Finishing Result Section

---
+++ result.js
@@ -0,0 +1,17 @@
+// Get divs
+const div1 = document.getElementById("result-table-container__item1");
+const div2 = document.getElementById("result-table-container__item2");
+
+document
+  .getElementById("result_wrapper__cnt--part1_btn1")
+  .addEventListener("click", () => {
+    div1.classList.add("active");
+    div2.classList.remove("active");
+  });
+
+document
+  .getElementById("result_wrapper__cnt--part1_btn2")
+  .addEventListener("click", () => {
+    div1.classList.remove("active");
+    div2.classList.add("active");
+  });
