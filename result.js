a83432b3dfaa0ec1295edd318b7faa775256bb8b
Tawhid Islam <xcode.dev@yahoo.com>
2024-12-16 00:46:21 +0600

    Fixing result section #1

--- result.js
+++ result.js
@@ -1,12 +1,14 @@
 // Get divs
 const div1 = document.getElementById("result-table-container__item1");
 const div2 = document.getElementById("result-table-container__item2");
+const div3 = document.getElementById("result_statistics_box");
 
 document
   .getElementById("result_wrapper__cnt--part1_btn1")
   .addEventListener("click", () => {
     div1.classList.add("active");
     div2.classList.remove("active");
+    div3.classList.remove("active");
   });
 
 document
@@ -14,4 +16,13 @@ document
   .addEventListener("click", () => {
     div1.classList.remove("active");
     div2.classList.add("active");
+    div3.classList.remove("active");
+  });
+
+document
+  .getElementById("result_statistics-btn")
+  .addEventListener("click", () => {
+    div1.classList.remove("active");
+    div2.classList.remove("active");
+    div3.classList.add("active");
   });
